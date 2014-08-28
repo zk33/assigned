3114e404c7fc8348f2808d3346a3e9ee99b02184
zk33 <ymzktkak@gmail.com>
2014-08-28 09:58:45 +0900

    Add reload funciton fixes #1

--- public/app.js
+++ public/app.js
@@ -3,19 +3,21 @@ OAuth.initialize(OAUTH_ID);
 
 angular.module('App', [])
   .controller('MainController',['$scope', function($scope){
+
     var api,orgCount;
+
     var getIssues = function(api,url){
       api.get(url)
-       .done(function(res){
-         $scope.$apply(function(){ $scope.issues = $scope.issues.concat(res); });
-         orgCount--;
-         if(orgCount<=0){
+      .done(function(res){
+        $scope.$apply(function(){ $scope.issues = $scope.issues.concat(res); });
+        orgCount--;
+        if(orgCount<=0){
            $scope.$apply(function(){ $scope.loggingin = false; $scope.loggedin = true; });
-         }
-       })
-       .fail(function(err){
-         $scope.$apply(function(){ $scope.error += ' ' + err; });
-       });
+        }
+      })
+      .fail(function(err){
+        $scope.$apply(function(){ $scope.error += ' ' + err; });
+      });
     };
 
     $scope.issues = [];
@@ -23,24 +25,33 @@ angular.module('App', [])
     $scope.error = "";
     $scope.loggingin = false;
     $scope.loggedin = false;
+
+    $scope.load = function(){
+      $scope.repos = [];
+      $scope.issues = [];
+
+      api.get('/user/orgs')
+      .done(function(res){
+        $scope.$apply(function(){ $scope.repos = res; });
+        getIssues(api,'/user/issues?filter=assigned');
+        orgCount = res.length + 1;
+        for(var i=0,len=res.length;i<len;i++){
+          getIssues(api,'/orgs/' + res[i].login + '/issues?filter=assigned');
+        }
+      });
+    };
+
     $scope.auth = function(){
       $scope.loggingin = true;
+
       OAuth.popup('github')
-        .done(function(result){
-          api = result;
-          api.get('/user/orgs')
-          .done(function(res){
-            $scope.$apply(function(){ $scope.repos = res; });
-            getIssues(api,'/user/issues?filter=assigned');
-            orgCount = res.length + 1;
-            for(var i=0,len=res.length;i<len;i++){
-              getIssues(api,'/orgs/' + res[i].login + '/issues?filter=assigned');
-            }
-          })
-        })
-        .fail(function(result){
-          $scope.error='Auth error';
-        });
+      .done(function(result){
+        api = result;
+        $scope.load();
+      })
+      .fail(function(result){
+        $scope.error='Auth error';
+      });
     };
   }]);
 
