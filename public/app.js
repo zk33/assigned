8365f1ba083268bbb646ac59d9ab8d1065736ddd
zk33 <ymzktkak@gmail.com>
2014-08-28 09:38:04 +0900

    minor fix

--- public/app.js
+++ public/app.js
@@ -3,11 +3,15 @@ OAuth.initialize(OAUTH_ID);
 
 angular.module('App', [])
   .controller('MainController',['$scope', function($scope){
-    var api;
+    var api,orgCount;
     var getIssues = function(api,url){
       api.get(url)
        .done(function(res){
          $scope.$apply(function(){ $scope.issues = $scope.issues.concat(res); });
+         orgCount--;
+         if(orgCount<=0){
+           $scope.$apply(function(){ $scope.loggingin = false; $scope.loggedin = true; });
+         }
        })
        .fail(function(err){
          $scope.$apply(function(){ $scope.error += ' ' + err; });
@@ -17,7 +21,10 @@ angular.module('App', [])
     $scope.issues = [];
     $scope.repos = [];
     $scope.error = "";
+    $scope.loggingin = false;
+    $scope.loggedin = false;
     $scope.auth = function(){
+      $scope.loggingin = true;
       OAuth.popup('github')
         .done(function(result){
           api = result;
@@ -25,6 +32,7 @@ angular.module('App', [])
           .done(function(res){
             $scope.$apply(function(){ $scope.repos = res; });
             getIssues(api,'/user/issues?filter=assigned');
+            orgCount = res.length + 1;
             for(var i=0,len=res.length;i<len;i++){
               getIssues(api,'/orgs/' + res[i].login + '/issues?filter=assigned');
             }
